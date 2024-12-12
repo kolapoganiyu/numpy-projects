eac763db55be1c7de2483afba5e33b86e7bda169
Kolapo <kolapsganiyu@gmail.com>
2024-12-12 19:35:33 +0100

    still working on numpy

---
+++ numpy-excercises/first/numpyrandomexcercises.py
@@ -0,0 +1,38 @@
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Generate a random integer from 0 to 100
+x=random.randint(100, size=( 5))
+print(x)
+
+# Generating a 3 rows of 5 random integers from 0 to 100
+x = random.randint(100, size=(3, 5))
+print(x)
+
+# Generating 3 rows of 5 random integer from the inserted list
+x = random.choice([5,6,7,8,10], size=(3,5))
+print(x)
+
+# Generating random float numbers
+x = random.rand()
+print(x)
+
+# Generating random numbers using probability density
+x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(10))
+print(x)
+
+# shuffling the array itself
+random.shuffle(x)
+print(x)
+
+# making the permutation of the array
+print(random.permutation(x))
+
+
+# plotting a DistPlot
+sns.distplot([0,1,2,3,4,5], hist=False)
+
+plt.show()
+
+
