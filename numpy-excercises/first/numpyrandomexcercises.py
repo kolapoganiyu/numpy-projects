a934c8fce62a5b267242dcc47192daca3e72349a
Kolapo <kolapsganiyu@gmail.com>
2024-12-24 23:21:20 +0100

    still working on the numpy  project

--- numpy-excercises/first/numpyrandomexcercises.py
+++ numpy-excercises/first/numpyrandomexcercises.py
@@ -35,4 +35,16 @@ sns.distplot([0,1,2,3,4,5], hist=False)
 
 plt.show()
 
+# Differences between normal and Binomial Distribution
+
+sns.distplot(random.normal(loc=50, scale=5, size=100,), hist=False, label="normal")
+sns.distplot(random.binomial(n=100,p=0.5, size=1000), hist=False, label="binomial")
+
+plt.show()
+
+# poisson Distribution
+
+sns.distplot(random.poisson(lam=2, size=1000), kde=False)
+
+plt.show()
 
